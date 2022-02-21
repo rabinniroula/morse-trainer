92874803823037ef1c687a5710c3effd9c8bd527
rabinniroula <30528863+rabinniroula@users.noreply.github.com>
2022-02-21 21:55:13 +0545

    uploading everything

---
+++ script.js
@@ -0,0 +1,46 @@
+let pretime
+let endtime
+var fired = false
+var ended = false
+var disp = document.getElementById('cw-display')
+var textarea = document.getElementById('decodedtext')
+var dotdasharray = []
+
+var beep = new p5.Oscillator('sine');
+beep.freq = 440
+beep.amp = 1
+
+setInterval(() => {
+    if ((Date.now() - endtime) > 700) {
+        dotdash = dotdasharray.join('')
+        disp.innerHTML = dotdash
+        if (cwTable.indexOf(dotdash) != -1) textarea.value += String.fromCharCode(cwTable.indexOf(dotdash) + 65)
+        dotdasharray = []
+    }
+}, 500)
+
+document.addEventListener("keydown", (e) => {
+    if (e.key == ' ' && !fired) {
+        beep.start()
+        fired = true
+        pretime = Date.now()
+    }
+})
+
+document.addEventListener("keyup", (e) => {
+    if (e.key == ' ') {
+        beep.stop()
+        fired = false
+            // console.log(`You pressed the space button for ${Date.now() - pretime}`)
+        checkdotdash(Date.now() - pretime)
+    }
+    endtime = Date.now()
+})
+
+function checkdotdash(time) {
+    if (time > 230) {
+        dotdasharray.push('-')
+    } else {
+        dotdasharray.push('.')
+    }
+}
